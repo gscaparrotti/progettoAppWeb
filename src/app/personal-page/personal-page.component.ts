6baf2693862d098177dd8e50b389b37a53f354e7
giacomo <giacomo.scaparrotti@studio.unibo.it>
2019-04-01 20:27:58 +0200

    added file upload gui in personal page

--- src/app/personal-page/personal-page.component.ts
+++ src/app/personal-page/personal-page.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ServerinteractorService} from '../serverinteractor.service';
-import {LegalAssistance, User} from '../model/model';
+import {User} from '../model/model';
 import {Option, option, none} from 'ts-option';
+import * as HttpStatus from 'http-status-codes';
 
 @Component({
   selector: 'app-personal-page',
@@ -12,7 +13,12 @@ export class PersonalPageComponent implements OnInit {
 
   user: User;
   lastRequestIndex = 0;
-  error: Option<string> = none;
+  userInfoError: Option<string> = none;
+  filesListError: Option<string> = none;
+  fileToUpload: File = null;
+  communicatingFiles = false;
+  fileUploadStatus = '';
+  alreadyUploadedFiles: string[] = [];
 
   constructor(private serverInteractorService: ServerinteractorService) { }
 
@@ -20,7 +26,7 @@ export class PersonalPageComponent implements OnInit {
     this.serverInteractorService.getUserInfo(sessionStorage.getItem('codicefiscale')).subscribe(
       result => {
         if (result != null) {
-          this.error = none;
+          this.userInfoError = none;
           this.user = result;
           // at the moment we only show the last request
           this.user.requiredLegalAssistance.forEach((request, index) => {
@@ -28,11 +34,49 @@ export class PersonalPageComponent implements OnInit {
               this.lastRequestIndex = index;
             }
           });
+          this.updateFilesList();
         } else {
-          this.error = option('Impossibile recuperare le informazioni sull\'utente.');
+          this.userInfoError = option('Impossibile recuperare le informazioni sull\'utente.');
         }
       },
-      e => this.error = option('Impossibile recuperare le informazioni sull\'utente.'));
+      () => this.userInfoError = option('Impossibile recuperare le informazioni sull\'utente.'));
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileUploadStatus = '';
+    this.fileToUpload = files.item(0);
+    this.fileUploadStatus = 'File selezionato: ' + this.fileToUpload.name;
+  }
+
+  uploadFile() {
+    if (this.fileToUpload != null) {
+      this.serverInteractorService.sendFile(sessionStorage.getItem('codicefiscale'),
+        this.user.requiredLegalAssistance[0].id, this.fileToUpload)
+        .subscribe(() => {
+          this.fileUploadStatus = 'Il file ' + this.fileToUpload.name + ' è stato inviato con successo';
+          this.updateFilesList();
+          this.fileToUpload = null;
+        }, error => {
+          if (error.status === HttpStatus.CONFLICT) {
+            this.fileUploadStatus = 'Impossibile inviare il file (possibile file duplicato)';
+          } else {
+            this.fileUploadStatus = 'Errore nell\'invio del file';
+          }
+          this.fileToUpload = null;
+        });
+    }
+  }
+
+  updateFilesList() {
+    this.communicatingFiles = true;
+    this.serverInteractorService.uploadedFilesList(this.user.codicefiscale,
+      this.user.requiredLegalAssistance[this.lastRequestIndex].id).subscribe(fileResult => {
+      this.alreadyUploadedFiles = fileResult;
+      this.communicatingFiles = false;
+    }, () => {
+      this.filesListError = option('Impossibile ottenere la lista dei file.');
+      this.communicatingFiles = false;
+    });
   }
 
 }
